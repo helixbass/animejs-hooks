3b8160a20af071957cb14e18ec2882e25b7e41ff
Julian Garnier <hello@julian.gr>
2016-06-30 12:43:17 +0200

    Revert "backward loops test"
    This reverts commit c007d50d2efea03e96048bc9d0130ab96c8380ba.

--- anime.js
+++ anime.js
@@ -465,13 +465,11 @@
     var transforms = undefined;
     anim.time = Math.min(time, anim.duration);
     anim.progress = (anim.time / anim.duration) * 100;
-    for (var t = anim.tweens.length-1; t >= 0; t--) {
-    // for (var t = 0; t < anim.tweens.length; t++) {
+    for (var t = 0; t < anim.tweens.length; t++) {
       var tween = anim.tweens[t];
       tween.currentValue = getTweenProgress(tween, time);
       var progress = tween.currentValue;
-      // for (var a = 0; a < tween.animatables.length; a++) {
-      for (var a = tween.animatables.length-1; a >= 0; a--) {
+      for (var a = 0; a < tween.animatables.length; a++) {
         var animatable = tween.animatables[a];
         var id = animatable.id;
         switch (tween.type) {
@@ -513,7 +511,7 @@
 
   var loop = (function() {
     var step = function() {
-      for (var a = animations.length-1; a >= 0; a--) animations[a].tick(raf);
+      for (var i = 0; i < animations.length; i++) animations[i].tick(raf);
       raf = requestAnimationFrame(step);
     }
     return {
