8a7db0268420c6d270242a14cd392273a16b2482
Julian Garnier <hello@julian.gr>
2017-02-18 20:06:34 +0100

    Clean up build file

--- build.js
+++ build.js
@@ -1,22 +1,21 @@
 const fs = require('fs');
 const compile = require('google-closure-compiler-js').compile;
 
+const fileIn = 'anime.js';
+const fileOut = 'anime.min.js';
+
 console.info('Compiling... 😤');
 
-fs.unlink('anime.min.js', (err) => {
-
-  fs.readFile('anime.js', {encoding: 'utf-8'}, function(err, data) {
+fs.unlink(fileOut, (err) => {
+  fs.readFile(fileIn, {encoding: 'utf-8'}, function(err, data) {
     if (err) throw err;
-    
     const flags = {
       jsCode: [{src: data}],
       languageIn: 'ES6',
       languageOut: 'ES5'
     };
-
     const out = compile(flags);
-
-    fs.writeFile('anime.min.js', out.compiledCode, function(err) {
+    fs.writeFile(fileOut, out.compiledCode, function(err) {
       if (err) throw err;
       console.info('Compilation was a success! 😎 🍺');
     });
